a95cef9e8ed7c1a56333b2e59d6ceae6631912ab
Aayush Sharma <aayushsharma4633@gmail.com>
2026-04-10 01:01:29 +0530

    Event in Js

---
+++ Event/event.js
@@ -0,0 +1,14 @@
+let btn1 = document.querySelector('#btn1');
+btn1.onclick = () =>{
+  console.log("btn1 was clicked");
+  let a = 25;
+  a++;
+  console.log(a);//26
+
+};
+
+let box = document.querySelector('div');
+
+box.onmouseover = () =>{
+  console.log("You are inside the div");
+}
